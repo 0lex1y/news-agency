a333d878787eeffe83345b2944377f1ac1af4b4c
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-10-23 20:22:28 +0300

    Create urls.py in news, added models and admin to news app.

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from news.models import Newspaper, Topics, Redactor
 
-# Register your models here.
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ['title','topic', 'publisher', 'published_date']
+    filter = ['published_date']
+    search_fields = ['title', 'topic', 'publisher']
+
+
+@admin.register(Topics)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    filter = ['name']
+
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email']
+    filter = ['username']
+    search_fields = ['username', 'first_name', 'last_name']
--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
-# Create your models here.
+class Topics(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topics, on_delete=models.CASCADE, related_name='newspapers')
+    publisher = models.ManyToManyField('Redactor', related_name='newspapers')
+
+    def __str__(self):
+        return f'{self.title} ({self.topic} - {self.publisher})'
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.username} ({self.first_name} {self.last_name})'

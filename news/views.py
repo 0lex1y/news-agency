3563e8746eae32fa30e4b4fb6a1feb134cc9cb2e
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-10-25 13:41:11 +0300

    Add sidebar.html to project and fix attr in admin.py for AbstractUser

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+class NewsIndex(generic.ListView):
+    model = Newspaper
+    template_name = 'news/index.html'
+    context_object_name = 'newspapers'
+    paginate_by = 5
+

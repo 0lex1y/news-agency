f6c4dfcdc338106e84dc32c68463429740762e5b
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-11-04 13:16:48 +0200

    update settings and requirements.txt

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-uqfj+uq*l&8a$7ie26xe-nco7lbyie1zk4^7$b3qfy^hl$jwl5"
-)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-uqfj+uq*l&8a$7ie26xe-nco7lbyie1zk4^7$b3qfy^hl$jwl5")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
@@ -125,7 +126,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "news:index"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "logout"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
